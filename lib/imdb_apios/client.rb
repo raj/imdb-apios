94882b9da7ed9488a2a4b3d15d5c270b2a573866
raj <raj.deenoo@gmail.com>
2018-03-02 23:08:03 +0000

    re enable test

--- lib/imdb_apios/client.rb
+++ lib/imdb_apios/client.rb
@@ -12,50 +12,9 @@ module ImdbApios
       ImdbApios::Constant::SIMPLE_GET_ENDPOINTS.each do |key, v|
         define_singleton_method :"#{key}" do |value|
           res = get_resource(v, value)
-          puts res.keys
           return res
         end
       end
-    end
-
-    def get_title_plot_synopsis imdb_id
-      return ""
-    end
-
-    def get_title_plot imdb_id
-    end
-
-    def get_title_user_reviews imdb_id
-    end
-
-    def get_title_metacritic_reviews imdb_id
-    end
-
-    def title_exists imdb_id
-    end
-
-
-    def search_for_name name
-    end
-
-
-    def search_for_title title
-    end
-
-    def get_popular_titles
-
-    end
-
-    def get_popular_shows
-
-    end
-
-    def get_popular_movies
-
-    end
-
-    def get_title_episodes imdb_id
-
     end
 
     private
